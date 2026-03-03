51e4bcc350cd3ef6401ac3d8a5ba8997e67d88ab
abdu <abdualkerim01@gmail.com>
2026-03-03 10:57:34 +0300

    email field added

--- app/_components/LoginForm.tsx
+++ app/_components/LoginForm.tsx
@@ -1,5 +1,18 @@
 // use client component
 "use client" 
 export default function LoginForm() {
-  return 
+  return (
+   <form action="" className="space-x-4">
+    <div>
+      <label className="block text-sm font-medium text-gray-700">Email</label>
+      <input 
+        type="email" 
+        name="email" 
+        placeholder=" enter ur email" 
+        required 
+        className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50" 
+/>
+    </div>
+   </form>
+  )
 }
