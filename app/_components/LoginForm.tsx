0dc8f088826c2f1e07e387a942e7be082f5a39f4
abdu <abdualkerim01@gmail.com>
2026-03-03 11:07:28 +0300

    password field added

--- app/_components/LoginForm.tsx
+++ app/_components/LoginForm.tsx
@@ -2,17 +2,29 @@
 "use client" 
 export default function LoginForm() {
   return (
-   <form action="" className="space-x-4">
-    <div>
-      <label className="block text-sm font-medium text-gray-700">Email</label>
-      <input 
-        type="email" 
-        name="email" 
-        placeholder=" enter ur email" 
-        required 
-        className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50" 
-/>
-    </div>
-   </form>
-  )
+    <form action="" className="space-x-4">
+      <div>
+        <label className="block text-sm font-medium text-gray-700">Email</label>
+        <input
+          type="email"
+          name="email"
+          placeholder=" enter ur email"
+          required
+          className="mt-2 p-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
+        />
+      </div>
+      <div className="mt-4">
+        <label className=" block text-sm font-medium text-gray-700">
+          Password
+        </label>
+        <input
+          type="passwrod"
+          name="password"
+          placeholder=" enter ur passwrod"
+          required
+          className="mt-2 p-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
+        />
+      </div>
+    </form>
+  );
 }
